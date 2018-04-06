88d8eace61c9d8257bfac3b727419e3a27f6c628
Mark Massoud <mmassoud@dynamite.ca>
2018-04-06 14:23:44 -0400

    Process booleans passed by CLI (convert string to boolean)

--- shared/utils.js
+++ shared/utils.js
@@ -8,6 +8,23 @@ module.exports = {
     this.options.stage = _.get(this, 'options.stage', 'dev');
     this.options.region = _.get(this, 'options.region', 'us-central1');
 
+    // Normalize booleans, since they're passed as strings
+    if (this.options.prependStage === 'false') {
+      this.options.prependStage = false;
+    }
+
+    if (this.options.prependStage === 'true') {
+      this.options.prependStage = true;
+    }
+
+    if (this.options.prependService === 'false') {
+      this.options.prependService = false;
+    }
+
+    if (this.options.prependService === 'true') {
+      this.options.prependService = true;
+    }
+
     const optPrependStage = _.get(this, 'options.prependStage');
     const optPrependService = _.get(this, 'options.prependService');
 
--- shared/utils.test.js
+++ shared/utils.test.js
@@ -42,6 +42,26 @@ describe('Utils', () => {
       });
     });
 
+    it('should prioritize options over provider options, even if booleans as strings', () => {
+      googleCommand.options.stage = 'my-stage';
+      googleCommand.options.region = 'my-region';
+      googleCommand.options.prependStage = 'false';
+      googleCommand.options.prependService = 'true';
+      googleCommand.serverless.service = {
+        provider: {
+          prependStage: true,
+          prependService: false,
+        },
+      };
+
+      return googleCommand.setDefaults().then(() => {
+        expect(googleCommand.options.stage).toEqual('my-stage');
+        expect(googleCommand.options.region).toEqual('my-region');
+        expect(googleCommand.options.prependStage).toEqual(false);
+        expect(googleCommand.options.prependService).toEqual(true);
+      });
+    });
+
     it('should prioritize options over provider options', () => {
       googleCommand.options.stage = 'my-stage';
       googleCommand.options.region = 'my-region';
