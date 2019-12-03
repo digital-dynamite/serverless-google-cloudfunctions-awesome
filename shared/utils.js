d76bfa97102f5e26bd11582bbc80919a7071b868
David Walsh <dwalsh@dynamite.ca>
2019-12-03 11:24:37 -0500

    Updated stage related code

--- shared/utils.js
+++ shared/utils.js
@@ -5,7 +5,7 @@ const BbPromise = require('bluebird');
 
 module.exports = {
   setDefaults() {
-    this.options.stage = _.get(this, 'options.stage', 'dev');
+    this.options.stage = _.get(this, 'options.stage') || _.get(this, 'serverless.service.provider.stage') || 'dev';
     this.options.region = _.get(this, 'options.region', 'us-central1');
     this.options.runtime = _.get(this, 'options.runtime', 'nodejs8');
 
